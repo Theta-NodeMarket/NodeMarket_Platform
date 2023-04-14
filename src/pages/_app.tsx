e3cb9b04f3cff3543ca2190e0254b88e579f8c3c
Alex Monteverde <55471655+Amonteverde04@users.noreply.github.com>
2023-04-13 22:50:29 -0400

    Adding auth to test page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -6,5 +9,13 @@ import "@fontsource/roboto/700.css";
 import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
