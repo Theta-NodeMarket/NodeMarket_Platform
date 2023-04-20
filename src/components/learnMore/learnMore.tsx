47c4a2e774a9aa18cde7199970de8f67a4689217
Alex Monteverde <alexmo@avi.com>
2023-04-20 19:26:33 -0400

    Making about and learn more consistent

--- src/components/learnMore/learnMore.tsx
+++ src/components/learnMore/learnMore.tsx
@@ -8,20 +8,17 @@ export default function LearnMore() {
       <div className={styles.learnMore_paragraph_wrapper}>
         <LearnMoreParagraph
           Heading="Decentralized Storage"
-          StanzaOne="We empower you to effortlessly manage your marketing campaigns without the hassle of media storage."
-          StanzaTwo="With our service, your media is accessible to your target audience, anytime and anywhere, thereby enabling you to focus on achieving your marketing goals."
+          StanzaOne="We empower you to effortlessly manage your marketing campaigns without the hassle of media storage. With our service, your media is accessible to your target audience, anytime and anywhere, thereby enabling you to focus on achieving your marketing goals."
         />
 
         <LearnMoreParagraph
           Heading="Distribute Globally"
-          StanzaOne="Harness the transformative potential of Web3 and concentrate on your key objectives."
-          StanzaTwo="Our service enables you to efficiently share your media with the aid of the Theta Network, thereby streamlining your operations and providing you with a competitive edge."
+          StanzaOne="Harness the transformative potential of Web3 and concentrate on your key objectives. Our service enables you to efficiently share your media with the aid of the Theta Network, thereby streamlining your operations and providing you with a competitive edge."
         />
 
         <LearnMoreParagraph
           Heading="Manage Seamlessly"
-          StanzaOne="Efficiently track, share, and promote your brand in a unified location with our comprehensive service. "
-          StanzaTwo="Our platform empowers you to focus on expanding your brand while we handle the logistics, enabling you to hit the ground running and achieve your business objectives."
+          StanzaOne="Efficiently track, share, and promote your brand in a unified location with our comprehensive service. Our platform empowers you to focus on expanding your brand while we handle the logistics, enabling you to hit the ground running and achieve your business objectives."
         />
       </div>
     </section>
--- src/components/learnMore/learnMoreParagraph.tsx
+++ src/components/learnMore/learnMoreParagraph.tsx
@@ -3,7 +3,6 @@ import styles from "./learnMoreParagraph.module.scss";
 interface learnMoreParagraphData {
   Heading: string;
   StanzaOne: string;
-  StanzaTwo: string;
 }
 
 export default function LearnMoreParagraph(props: learnMoreParagraphData) {
@@ -12,7 +11,6 @@ export default function LearnMoreParagraph(props: learnMoreParagraphData) {
       <h1 className={styles.heading}>{props.Heading}</h1>
       <div className={styles.stanza_wrap}>
         <p className={styles.body1}>{props.StanzaOne}</p>
-        <p className={styles.body2}>{props.StanzaTwo}</p>
       </div>
     </div>
   );
