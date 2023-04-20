757d5044f6c14373edc3d4061427a077a7620b41
Hecker <julianheckerdev@gmail.com>
2023-04-19 21:06:24 -0400

    add about us page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
-import type { AppProps } from "next/app";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "@/styles/globals.scss";
+import { useState } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import type { AppProps } from 'next/app';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/components/layout/layout';
+import React from 'react';
+import { About } from './About';
+
+export default function AboutPage() {
+  return (
+    <Layout>
+      <About />
+    </Layout>
+  );
+}
