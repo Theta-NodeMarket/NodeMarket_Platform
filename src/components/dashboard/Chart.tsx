3a74963e9c79f73def61743fc77cf99a21416681
Alex Monteverde <alexmo@avi.com>
2023-04-30 12:49:46 -0400

    Getting familiar with the chart, adding some styles, adding fake date data to see how it would be graphed.

--- src/components/dashboard/Chart.tsx
+++ src/components/dashboard/Chart.tsx
@@ -1,4 +1,5 @@
 import { ApexOptions } from "apexcharts";
+import { Card, Box } from "@mui/material";
 import dynamic from "next/dynamic";
 // Next dies when it tries to SSR ApexChart
 // https://github.com/apexcharts/react-apexcharts/issues/240#issuecomment-1077335256
@@ -17,6 +18,21 @@ export const ImpressionsAndClicksChart = ({
   series = defaultSeries,
 }: ImpressionsAndClicksChartProps) => {
   const options: ApexOptions = {
+    chart: {
+      foreColor:"#FAFAFA",
+      fontFamily: "Roboto",
+      toolbar: {
+        show: true,
+        tools: {
+          download: false,
+          selection: true,
+          zoom: true,
+          zoomin: true,
+          zoomout: true,
+          pan: true,
+        },
+      },
+    },
     yaxis: [
       {
         title: {
@@ -30,18 +46,27 @@ export const ImpressionsAndClicksChart = ({
         },
       },
     ],
-    // xaxis: {
-    //   type: "datetime",
-    // },
+    xaxis: {
+      categories: ['2002-03-01','2002-03-05','2002-03-06','2002-03-07', '2002-03-08', '2002-03-15', '2002-03-16', '2002-03-17', '2002-03-25', '2002-03-30'],
+    },
   };
 
   return (
-    <ApexChart
-      options={options}
-      series={series}
-      type="line"
-      width={500}
-      height={320}
-    />
+    <Card 
+      sx={{
+        background: "linear-gradient(rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05))",
+        border: "solid 1px rgba(250, 250, 250, .25)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+      <ApexChart
+        options={options}
+        series={series}
+        type="line"
+        width={500}
+        height={320}
+      />
+    </Card>
   );
 };
