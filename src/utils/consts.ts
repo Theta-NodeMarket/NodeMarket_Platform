d4d01ffb33ec1e7e8c4a88555f4429cf08b1b3a4
Hecker <julianheckerdev@gmail.com>
2023-05-22 00:12:00 -0400

    fix broken theta upload

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,13 +2,14 @@ export const adTable = "advertisements";
 export const campaignTable = "campaigns";
 export const promoTable = "promotions";
 
-export const ADVERTISER_DETAILS_NO_DATA_WARNING = "Your advertisement has not generated any impressions or clicks thus far."
-export const ADVERTISER_DASHBOARD_NO_DATA_WARNING = `If you haven't created any advertisements yet, please click the "CREATE NEW ADVERTISEMENT" button on the dashboard to add a new advertisement. Once your advertisement is approved and begins to generate clicks and impressions, the corresponding metrics will be presented here.`
+export const ADVERTISER_DETAILS_NO_DATA_WARNING =
+  "Your advertisement has not generated any impressions or clicks thus far.";
+export const ADVERTISER_DASHBOARD_NO_DATA_WARNING = `If you haven't created any advertisements yet, please click the "CREATE NEW ADVERTISEMENT" button on the dashboard to add a new advertisement. Once your advertisement is approved and begins to generate clicks and impressions, the corresponding metrics will be presented here.`;
 export const ADVERTISER_NO_DATA_WARNING = `Currently, there are no metrics available for display.`;
 export const PROMOTER_NO_DATA_WARNING = `Currently, there are no metrics available for display. To incorporate advertisements on your website, kindly refer to the documentation page for detailed instructions. Once your website starts showcasing advertisements and users interact with them, the corresponding metrics will be visible in this section.`;
 
 export const thetaDataUrl = "https://data.thetaedgestore.com/api/v2/data/";
-export const thetaUploadUrl = "https://api.thetaedgestore.com/api/v2/data/";
+export const thetaUploadUrl = "https://api.thetaedgestore.com/api/v2/data";
 
 export const acceptedFileTypes = [
   "png",
